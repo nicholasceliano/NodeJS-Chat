371482e3da63ec189700ca4b9817580382996fc5
nicholasceliano <nicholasceliano@yahoo.com>
2014-06-04 15:28:46 -0400

    checkin

--- nodetest1/public/javascripts/chat.js
+++ nodetest1/public/javascripts/chat.js
@@ -1,9 +1,20 @@
-var chat = {
+var userName;
 
+var chat = {
+	joinChat: function(){
+		userName = $('#txtUsername').val();
+		$('#joinChat').hide();
+		$('#inChat').show();
+	},
+	leaveChat:function(){
+		$('#txtUsername').val('');
+		$('#joinChat').show();
+		$('#inChat').hide();
+	},
 	addText: function(){
 		var txtIn = $('#txtChatInput').val();
 		
-		$('#chatArea').append('<br />' + txtIn);
+		$('#chatArea').append('<br />' + userName + ': ' + txtIn);
 		
 		chat.scrollToBottom($('#chatArea'));
 	},
